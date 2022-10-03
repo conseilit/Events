165098cd4547c29695facf33a81e584a1a40a62d
Christophe Laporte <35958968+conseilit@users.noreply.github.com>
2022-10-03 15:01:39 +0200

    Update 05 - Configure To diagnose TempDB AutoGrowth.ps1

--- DataPlatformSummit 2022/Demos/05 - Configure To diagnose TempDB AutoGrowth.ps1
+++ DataPlatformSummit 2022/Demos/05 - Configure To diagnose TempDB AutoGrowth.ps1
@@ -29,7 +29,7 @@ Invoke-DbaQuery -SqlInstance $Server -Database "master" -Query "
     ADD EVENT sqlserver.database_file_size_change(
         ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_id,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)
         WHERE ([database_id]=(2) AND [session_id]>(50))),
-    ADD EVENT sqlserver.databases_log_file_used_changed(
+    ADD EVENT sqlserver.databases_log_file_size_changed(
         ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_id,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)
         WHERE ([database_id]=(2) AND [session_id]>(50)))
     ADD TARGET package0.event_file(SET filename=N'TempDBAutogrowth',max_file_size=(50),max_rollover_files=(10))
